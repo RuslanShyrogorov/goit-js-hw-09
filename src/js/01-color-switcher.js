1441a0a468bdac6c0df0ebbb794ab9db1cd97b00
RuslanShyrogorov <ruslanshirogorov@gmail.com>
2022-08-01 01:31:25 +0300

    finish task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,35 @@
+const refs = {
+  startBtn: document.querySelector('button[data-start]'),
+  stopBtn: document.querySelector('button[data-stop]'),
+  body: document.querySelector('body')
+}
 
+let intervalId = null
+let isActive = false
+const DELAY_INTERVAL = 1000
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+refs.startBtn.addEventListener('click', handlerStartBtn)
+
+function handlerStartBtn() {
+  if (isActive) {
+    return
+  }
+
+  isActive = true
+
+  intervalId = setInterval(() => {
+    refs.body.style.backgroundColor = getRandomHexColor()
+  }, DELAY_INTERVAL)
+}
+
+refs.stopBtn.addEventListener('click', handlerStopBtn)
+
+function handlerStopBtn() {
+  clearInterval(intervalId)
+
+  isActive = false
+}
---
+++ src/js/02-timer.js
@@ -0,0 +1,24 @@
+import flatpickr from "flatpickr";
+import "flatpickr/dist/flatpickr.min.css";
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+
+// flatpickr("#datetime-picker", {
+//   enableTime: true,
+//   time_24hr: true,
+//   defaultDate: new Date(),
+//   minuteIncrement: 1,
+//   onClose(selectedDates) {
+//     console.log(selectedDates[0]);
+//   }
+// });
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    console.log(selectedDates[0]);
+  },
+};
